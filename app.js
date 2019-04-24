34cdfcceb4d6cb5c14e839e89c885f3a1354c962
Daniele Vinci <vinci.dani@gmail.com>
2019-04-24 13:45:16 -0400

    implemented the hard coded routes

--- app.js
+++ app.js
@@ -5,11 +5,29 @@ var app = express();
 var routes = require('./routes');
 
 var jsonParser = require('body-parser').json;
+var logger = require('morgan');
 
+app.use(logger('dev'));
 app.use(jsonParser());
 
 app.use('/questions', routes);
 
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+// Custom Error Handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
